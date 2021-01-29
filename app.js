a8964602004a386f128f496ddca5f6c018026b77
TankistPro <nakol.nayk@mail.ru>
2021-01-29 22:39:22 +0300

    rewrite to Express.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.port || 3000;
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
+})
+
+app.get("/slider", ((req, res) => {
+  res.sendFile(__dirname + "/views/slider.html")
+}))
+
+
+app.listen(PORT, () => {
+  console.log(`[OK] Server is running. PORT: ${PORT}`)
+})
