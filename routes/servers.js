c0bdd2e74737ebd5c3231c71721b71e3a35a518e
TankistPro <nakol.nayk@mail.ru>
2021-01-29 23:50:14 +0300

    add slider fliters routes

--- routes/servers.js
+++ routes/servers.js
@@ -6,8 +6,10 @@ router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/../views/index.html"))
 })
 
-router.get("/slider", ((req, res) => {
-  res.sendFile(path.join(__dirname + "/../views/slider.html"))
+router.get("/slider/:filter", ((req, res) => {
+  const filter = req.params['filter'];
+  console.log(filter);
+  res.sendFile(path.join(__dirname + "/../views/slider.html"));
 }))
 
 module.exports = router;
