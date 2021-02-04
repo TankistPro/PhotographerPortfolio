bf25fab5e25ebcb1d8d2117c3f27d0b2378c7f66
TankistPro <nakol.nayk@mail.ru>
2021-02-04 20:08:49 +0300

    add animation order label

--- public/scripts/orderPopup.js
+++ public/scripts/orderPopup.js
@@ -14,3 +14,11 @@ closeOrder.addEventListener('click', () => {
   modalWrapper.style.display = 'none';
   body.style.overflow = 'auto';
 })
+
+function sendOrder(event) {
+  event.preventDefault();
+
+  const userName = document.forms['orderPopup'].elements['userName'].value,
+      phone = document.forms['orderPopup'].elements['userPhone'].value;
+
+}
