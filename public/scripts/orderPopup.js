8c5699d5ddf7032e675b49892b12b1ef8a3b67fe
TankistPro <nakol.nayk@mail.ru>
2021-02-04 19:18:10 +0300

    add orderPopup body

--- public/scripts/orderPopup.js
+++ public/scripts/orderPopup.js
@@ -1,13 +1,16 @@
 const modalWrapper = document.querySelector(".modal-wrapper"),
     orderBtn = document.querySelectorAll('.order-btn'),
-    closeOrder = document.querySelector('.modal-block__close');
+    closeOrder = document.querySelector('.modal-block__close'),
+    body = document.querySelector('body');
 
 orderBtn.forEach(button => {
   button.addEventListener('click', () => {
     modalWrapper.style.display = 'flex';
+    body.style.overflow = 'hidden';
   })
 })
 
 closeOrder.addEventListener('click', () => {
   modalWrapper.style.display = 'none';
+  body.style.overflow = 'auto';
 })
