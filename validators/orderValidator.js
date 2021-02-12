c5e25355fd8276fec15dcb1c022c0e18205918f9
TankistPro <nakol.nayk@mail.ru>
2021-02-12 21:11:27 +0300

    fix

--- validators/orderValidator.js
+++ validators/orderValidator.js
@@ -3,5 +3,4 @@ const  { check } = require('express-validator');
 module.exports.orderValidator = [
     check('userName').isLength({min: 2}).not().isNumeric(),
     check('phone').matches(/^\([0-9]{3}\)[0-9]{3}-[0-9]{2}-[0-9]{2}$/)
-]
-// .isLength({min:14, max:14})
+];
